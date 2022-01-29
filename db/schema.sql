77042889499591d071178c7806c142d263bc8030
hy <yang.hrn@gmail.com>
2022-01-29 22:18:43 +0000

    database col

--- db/schema.sql
+++ db/schema.sql
@@ -1,1 +1,29 @@
-DROP DATABASE IF EXISTS 
+DROP DATABASE IF EXISTS hr_db;
+CREATE DATABASE hr_db;
+USE hr_db;
+
+CREATE TABLE departments (
+    id INT NOT NULL PRIMARY KEY,
+    name VARCHAR(30)
+);
+
+CREATE TABLE roles (
+    id INT NOT NULL PRIMARY KEY,
+    title VARCHAR(30),
+    salary DECIMAL,
+    department_id INT,
+    FOREIGN KEY (department_id)
+    REFERENCES departments(id)
+    ON DELETE SET NULL
+);
+
+CREATE TABLE employee (
+    id INT NOT NULL,
+    first_name VARCHAR(30),
+    last_name VARCHAR(30),
+    role_id INT,
+    manager_id INT,
+    FOREIGN KEY (role_id)
+    REFERENCES roles(id)
+    ON DELETE SET NULL
+);
